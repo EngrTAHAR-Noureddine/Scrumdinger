d413465087385bc7958060657dae057608faa1e2
Engr.TAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-08-17 12:51:31 +0100

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by clever zone on 17/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
